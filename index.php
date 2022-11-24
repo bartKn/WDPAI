d847235848dff58358a2f775368713ad2b971b44
Bartosz Kniaziewicz <605bart@gmail.com>
2022-11-24 21:37:32 +0100

    Routing

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Test';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('calendar', 'DefaultController');
+Routing::get('event', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('mainpage', 'DefaultController');
+Routing::get('profile', 'DefaultController');
+Routing::get('signup', 'DefaultController');
+Routing::get('team', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+    
+    public function index() {
+        $this->render('login');
+    }
+
+    public function calendar() {
+        $this->render('calendar');
+    }
+
+    public function event() {
+        $this->render('event');
+    }
+
+    public function login() {
+        $this->render('login');
+    }
+
+    public function mainpage() {
+        $this->render('mainpage');
+    }
+
+    public function profile() {
+        $this->render('profile');
+    }
+
+    public function signup() {
+        $this->render('signup');
+    }
+
+    public function team() {
+        $this->render('team');
+    }
+}
