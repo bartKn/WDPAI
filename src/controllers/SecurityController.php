3aa17b158fb77900aca2316aa2dac516fea99e28
Bartosz Kniaziewicz <605bart@gmail.com>
2022-12-09 13:49:33 +0100

    Database connection

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,12 +2,13 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new User('email@email.com', 'pass', 'John', 'Doe');
+        $userRepository = new UserRepository();
 
         if (!$this->isPost())
         {
@@ -16,6 +17,13 @@ class SecurityController extends AppController
 
         $email = $_POST["email"];
         $password = $_POST["password"];
+
+        $user = $userRepository->getUser($email);
+
+        if (!$user)
+        {
+            return $this->render('login', ['messages' => ['User with this email not exist!']]);
+        }
 
         if ($user->getEmail() !== $email)
         {
